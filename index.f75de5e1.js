7ce75b6f2dc6ab6a4e0339bba6d88c634bc3e9d3
Alex Popov <alexpopovdm@outlook.com>
2024-10-07 08:04:46 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,30 @@
+"use strict";
+const logo = document.querySelector(".logo");
+const message = document.createElement("div");
+const errorMessage = document.createElement("div");
+message.classList.add("message");
+errorMessage.classList.add("message", "error-message");
+message.innerHTML = "Promise was resolved!";
+errorMessage.innerHTML = "Promise was rejected!";
+const promise1 = new Promise((resolve)=>{
+    logo.addEventListener("click", ()=>{
+        resolve();
+    });
+});
+const promise2 = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        reject(new Error("Promise was rejected due to timeout"));
+    }, 3000);
+});
+promise1.then(()=>{
+    document.body.append(message);
+}).catch(()=>{
+    document.body.append(errorMessage);
+});
+promise2.then(()=>{
+    document.body.append(message);
+}).catch(()=>{
+    document.body.append(errorMessage);
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
